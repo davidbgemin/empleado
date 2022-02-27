70f1c74d11f21c27e9a964fd3657246bda93db09
david <davidbgemin@gmail.com>
2022-02-26 19:26:48 -0500

    add ip

--- empleado/settings/prod.py
+++ empleado/settings/prod.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['137.184.62.89']
 
 
 
